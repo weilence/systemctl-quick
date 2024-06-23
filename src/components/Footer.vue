ff5432fbbfa16cc677a3c785dd2eb4dfb70ad779
Weilence <weilence@163.com>
2024-06-23 11:34:31 +0800

    refactor: remove unocss attribute and webfont

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,15 +3,14 @@ import { isDark, toggleDark } from '~/composables'
 </script>
 
 <template>
-  <nav my-6 inline-flex gap-2 text-xl>
-    <button class="icon-btn !outline-none" @click="toggleDark()">
-      <div v-if="isDark" i-carbon-moon />
-      <div v-else i-carbon-sun />
-    </button>
+  <nav class="my-6 inline-flex gap-2 text-xl">
+    <div class="icon-btn cursor-pointer !outline-none" @click="toggleDark()">
+      <div v-if="isDark" class="i-carbon-moon" />
+      <div v-else class="i-carbon-sun" />
+    </div>
 
     <a
-      class="icon-btn"
-      i-carbon-logo-github
+      class="icon-btn i-carbon-logo-github"
       rel="noreferrer"
       href="https://github.com/Weilence/systemctl-quick"
       target="_blank"
